89a85a658a153f3009df91de29ceca11edc92328
MB <120386287+MBrugge@users.noreply.github.com>
2024-11-23 21:12:42 +0100

    Base setup ready

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    { path: '/', name: 'Home', component: () => import('@/views/Home.vue') },
+    { path: '/project/:id', name: 'Project', component: () => import('@/views/Projects.vue') },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import('@/views/NotFound.vue') }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
-})
+    history: createWebHistory(),
+    routes
+});
 
-export default router
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main>
+        <h1>Not Found</h1>
+    </main>
+</template>
