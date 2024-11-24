3c34d16b8c52f1dbc4e0ac582335c1ac0bfc8c10
MB <120386287+MBrugge@users.noreply.github.com>
2024-11-24 19:54:11 +0100

    Nav styling and component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import Navbar from '@/components/Navbar.vue'
 </script>
 
 <template>
-    <header class="flex justify-center items-center h-4">
-        <nav class="flex justify-center space-x-4">
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/projects">Projects</RouterLink>
-        </nav>
-    </header>
+    <Navbar />
 
     <main class="flex justify-center items-center min-h-screen">
         <RouterView />
---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <header class="fixed top-0 left-0 w-full z-50">
+        <nav class="flex justify-center space-x-8 p-4">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/projects">Projects</RouterLink>
+        </nav>
+    </header>
+</template>
