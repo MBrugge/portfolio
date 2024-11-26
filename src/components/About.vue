58377d33152e2ac5e8ffd75345c8d99065ce873a
MB <120386287+MBrugge@users.noreply.github.com>
2024-11-26 12:23:01 +0100

    About me finished for now

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import Separator from '@/components/ui/separator/Separator.vue';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
 
+// Accordion Logic
 const defaultValue = 'item-1'
+const accordionItems = computed(() => [
+    { value: 'item-1', title: 'Who am I?', content: `I am a ${age.value} year old software developer from the Netherlands. I am currently studying Software Developer at Alfa-College in Groningen.` },
+    { value: 'item-2', title: 'What can I work with?', content: 'I have varying degrees of experience in a variety of languages and frameworks such as: Vue, Svelte, Laravel, TS, JS HTML, CSS, Go.' },
+    { value: 'item-3', title: 'What have I done?', content: 'I have earned my MAVO-tl diploma. I have finished Cambridge at C2 level. I am currently working to earn my degree for Software Developer.' },
+]);
 
-const accordionItems = [
-    { value: 'item-1', title: 'Who am I?', content: 'Yes. It adheres to the WAI-ARIA design pattern.' },
-    { value: 'item-2', title: 'What can I work with?', content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.' },
-    { value: 'item-3', title: 'What have I done?', content: 'Yes! You can use the transition prop to configure the animation.' },
-]
+// Age logic
+const birthDate = new Date(2003, 10, 15); // Year, Month(0-11), and Day(1-31)
+const currentDate = new Date();
+
+const calculateAge = (birthDate: Date, currentDate: Date): number => {
+    let age = currentDate.getFullYear() - birthDate.getFullYear();
+    const monthDifference = currentDate.getMonth() - birthDate.getMonth();
+    const dayDifference = currentDate.getDate() - birthDate.getDate();
+
+    if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+        age--;
+    }
+    return age;
+};
+
+const age = computed(() => calculateAge(birthDate, currentDate));
 </script>
 
 <template>
@@ -18,7 +36,7 @@ const accordionItems = [
         <div class="w-full px-4 sm:w-2/5 relative z-10">
             <Separator />
         </div>
-        <Accordion type="single" class="w-full px-4 sm:w-2/5 relative z-10" collapsible :default-value="defaultValue">
+        <Accordion type="single" class="w-full px-4 sm:w-2/5 relative z-10 text-start" collapsible :default-value="defaultValue">
             <AccordionItem v-for="item in accordionItems" :key="item.value" :value="item.value">
                 <AccordionTrigger>{{ item.title }}</AccordionTrigger>
                 <AccordionContent>
