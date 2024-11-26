50fb69b8140a5227a2bef1cc8ed385a4688c8c8d
MB <120386287+MBrugge@users.noreply.github.com>
2024-11-26 14:09:09 +0100

    Contact and footer added

---
+++ src/components/Contact.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Separator from '@/components/ui/separator/Separator.vue';
+</script>
+
+<template>
+    <section id="contact"
+        class="min-h-screen w-full flex flex-col justify-center items-center text-center relative overflow-hidden">
+        <h1 class="text-5xl font-bold mb-4 relative z-10">Contact</h1>
+        <div class="w-full px-4 sm:w-2/5 relative z-10">
+            <Separator />
+        </div>
+        <div class="flex flex-col pt-2 space-y-2 relative z-10 sm:flex-row sm:space-y-0 sm:space-x-4">
+            <p class="text-xl">Feel free to contact me if you are interested!</p>
+        </div>
+        <div class="mt-6 flex flex-col items-center space-y-4 relative z-10">
+            <a href="https://www.linkedin.com/in/marten-brugge" target="_blank" class="w-48 px-4 py-2 font-bold border border-gray-300 rounded-lg hover:bg-customBlue">LinkedIn</a>
+            <a href="mailto:martenbrugge@gmail.com" class="w-48 px-4 py-2 font-bold border border-gray-300 rounded-lg hover:bg-customBlue">Email Me</a>
+        </div>
+    </section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import Hero from '@/components/Hero.vue';
 import About from '@/components/About.vue'
+import Contact from '@/components/Contact.vue';
 </script>
 
 <template>
     <Hero />
     <About />
+    <Contact />
+    <footer class="w-full py-4 text-center">
+        <p>&copy; 2024 Marten Brugge. All rights reserved.</p>
+    </footer>
 </template>
