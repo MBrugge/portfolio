ea109d120e125c90c1d3c566419fda1ebbbb7f3c
MB <120386287+MBrugge@users.noreply.github.com>
2024-11-27 10:02:41 +0100

    First done

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import projectsData from '@/data/projects.json';
+import { ref } from 'vue';
 
+const projects = ref(projectsData);
 </script>
 
 <template>
-    <h1>Projects</h1>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Card v-for="project in projects" :key="project.id">
+            <CardHeader>
+                <CardTitle>{{ project.name }}</CardTitle>
+                <CardDescription>{{ project.framework }}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                {{ project.description }}
+            </CardContent>
+            <CardFooter>
+
+            </CardFooter>
+        </Card>
+    </div>
 </template>
